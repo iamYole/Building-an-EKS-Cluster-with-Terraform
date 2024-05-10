d4c2f240c6521801f5229ea9aae5260fa49ecd2a
Gideon <gideonovuzorie@gmail.com>
2024-05-10 06:14:07 +0100

    Updated the cluster version

--- main.tf
+++ main.tf
@@ -1,12 +1,24 @@
 module "eks_cluster" {
   source                          = "terraform-aws-modules/eks/aws"
-  version                         = "~> 18.0"
+  version                         = "~> 20.0"
   cluster_name                    = var.cluster_name
-  cluster_version                 = "1.22"
+  cluster_version                 = "1.29"
   vpc_id                          = module.vpc.vpc_id
   subnet_ids                      = module.vpc.private_subnets
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
   # Self Managed Node Group(s)
   self_managed_node_group_defaults = {
